289475f2cfe8d3e32331ede6ec0dd7059c5f76ae
Nick Prokhorov <irider.pro@yandex.ru>
2022-03-15 00:18:15 +0300

    first release

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path("v1/", include("djoser.urls")),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path("v1/", include("djoser.urls.jwt")),
 ]
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,13 +4,25 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField("имя", max_length=200)
+    slug = models.SlugField("адрес", unique=True)
+    description = models.TextField("описание")
+
+    class Meta:
+        verbose_name = "Groups, it will be shown in admin panel"
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        User, on_delete=models.CASCADE, related_name="posts"
+    )
+    image = models.ImageField(upload_to="posts/", null=True, blank=True)
 
     def __str__(self):
         return self.text
@@ -18,9 +30,36 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
     text = models.TextField()
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        "Дата добавления", auto_now_add=True, db_index=True
+    )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="follower",
+        verbose_name="кто подписывается",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="following",
+        verbose_name="на кого подписываютс",
+    )
+
+    class Meta:
+        unique_together = ("user", "author")
+        constraints = [
+            models.UniqueConstraint(
+                name="prevent_self_following",
+                fields=["user", "author"],
+            ),
+        ]
